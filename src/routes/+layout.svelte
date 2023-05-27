de10b39ebf848a2582398b1bcb71511f1eac3c4f
trinayv <127184886+trinayv@users.noreply.github.com>
2023-05-27 08:25:14 +0000

    header font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Public+Sans:wght@500&family=Raleway:wght@200&family=Source+Sans+Pro:wght@600&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/d0c488930b.js" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous"/>
+<link href="https://fonts.googleapis.com/css2?family=Peralta&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous"/>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Peralta&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/d54b205a8a.js" crossorigin="anonymous"></script>
+
+</svelte:head>
+
+<slot />
+
+<style>
+	:global(*) {
+        font-family: "Nunito Sans", sans-serif;
+        margin: 0;
+        padding: 0;
+}
+	}
+	:global(html, body) {
+		width: 100%;
+		height: 100%;
+	}
+
+</style>
