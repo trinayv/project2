33e5ea1b5a0fff13137759b6577465fe3780b915
trinayv <127184886+trinayv@users.noreply.github.com>
2023-05-27 08:26:45 +0000

    add hmtl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,74 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="readable-area">
+  <div class="header">
+    <div style="background-color:#f4f4f4;padding:16px;margin:32px; width: 257px;height:746px;float: left;">
+      <a href="https://i.pinimg.com/564x/08/a0/52/08a052d80710e6bea5f44cbc00088b99.jpg"> <img src="https://i.pinimg.com/564x/08/a0/52/08a052d80710e6bea5f44cbc00088b99.jpg" title="Profile Picture" class="center">
+      </a>
+      <div class="header-text">
+        <h1> Nemo </h1>
+        <p class="job-title"> CLOWNFISH </p>
+      </div>
+      <div class="content">
+        <div class="content-column">
+          <div class="section-title">
+            <i class="fa-sharp fa-solid fa-location-dot title-icon"></i>
+            The Great Barrier Reef
+          </div>
+        </div>
+        <div class="content-column">
+          <div class="section-title">
+            <i class="fa-sharp fa-solid fa-envelope-open-text title-icon"> </i>
+            findingnemo@gmail.com
+          </div>
+        </div>
+        <div class="content-column">
+          <div class="section-title">
+
+            <i class="fa-solid fa-laptop title-icon"></i>
+            www.findingnemo.com
+          </div>
+        </div>
+      </div>
+
+      <h2 class="profile"> profile </h2>
+      <hr style="height:1.5px;border-width:0;color:black;background-color:gray">
+      <p class="profile-text"> Hi! I'm Nemo, born with one small fin. I'm a curious and impressionable six-year-old, only child who lives with my overprotective and single-parent father. Despite being born with a withered fin, I yearns for adventure, and as fate takes me far from home, I learns that I am capable of doing great things. </p>
+    </div>
+    <h2 class="profile"> skills </h2>
+    <hr class="hr2">
+    <li class="skill">
+      <a class="skill">
+        <span class="skill-title">Swimming Speed</span>
+        <div class="gauge">
+          <div class="bullet filled-bullet"></div>
+          <div class="bullet filled-bullet"></div>
+          <div class="bullet filled-bullet"></div>
+          <div class="bullet"></div>
+          <div class="bullet"></div>
+        </div>
+      </a>
+    </li>
+    <li class="skill">
+      <span class="skill-title">Physical Strength</span>
+      <div class="gauge">
+        <div class="bullet filled-bullet"></div>
+        <div class="bullet filled-bullet"></div>
+        <div class="bullet filled-bullet"></div>
+        <div class="bullet filled-bullet"></div>
+        <div class="bullet"></div>
+      </div>
+    </li>
+    <h2 class="profile"> education </h2>
+    <hr class="hr2">
+    <p class="profile-text">
+    <ul>
+      <li>
+        <span class="profile-text2">2003</span>
+        <span class="time">The School Fish</span>
+    </ul>
+    </p>
+    <h2 class="profile"> interest </h2>
+    <hr class="hr2">
+    <div class="section-title">
+      <i class="fa-solid fa-magnifying-glass"></i> Adventure and Exploring
+    </div>
+  </div>
